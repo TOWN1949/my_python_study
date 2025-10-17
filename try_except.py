1783750c79e0710fa7e632533af86f544d25496b
TOWN1949 <toyohoon@outlook.com>
2025-10-17 11:12:22 +0800

    2025年10月17日

--- try_except.py
+++ try_except.py
@@ -1,12 +1,14 @@
 i=0
-while (i<2):
+while (i<5):
  try:
     beichushu=int(input("请输入被除数："))
     chushu=int(input("请输入除数："))
     print('\n',beichushu,'/',chushu,'=',beichushu/chushu)
- except ValueError:
-   print(" \n请输入整数")
- except ZeroDivisionError:
-    print("\n除数不能为零，请重新输入！")
+#  except (ValueError,ZeroDivisionError):
+#     print(" \n请输入整数或者除数不能为零")
+#  except ZeroDivisionError:
+#    print("\n除数不能为零，请重新输入！")
+ except(ZeroDivisionError,ValueError) as e:
+   print("输入错误:",e)
  i=i+1
     
